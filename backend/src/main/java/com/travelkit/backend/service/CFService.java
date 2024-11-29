6d41ea1ba8b038ffdbae66404ba9fe4d8a2a7d0f
minwoo <minwoo5377@naver.com>
2024-11-29 12:51:36 +0900

    BE : [feat] 단어 의미형태 유사도 계산

--- backend/src/main/java/com/travelkit/backend/service/CFService.java
+++ backend/src/main/java/com/travelkit/backend/service/CFService.java
@@ -5,12 +5,22 @@ import com.travelkit.backend.domain.Checklist;
 import com.travelkit.backend.domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
+import java.io.File;
 
 @Service
 public class CFService {
+    String modelPath = "C:\\Users\\minwo\\Documents\\word2vec\\word2vec_model_korean.txt";  // 실제 경로로 변경
+    // 모델 로드
+    File modelFile = new File(modelPath);
+    WordVectors wordVectors = WordVectorSerializer.loadStaticModel(modelFile);
     private final ChecklistRepository checklistRepository;
 
     @Autowired
@@ -18,11 +28,11 @@ public class CFService {
         this.checklistRepository = checklistRepository;
     }
 
-    public  Map<String, Double> recommendItems(Long checklistId) {
+    public Map<String, Double> recommendItems(Long checklistId) {
         // 사용자별 준비물 매트릭스 생성
         Map<Long, Set<String>> userItemsMap = new HashMap<>();
         Checklist target = checklistRepository.findById(checklistId).orElse(null);
-        if (target == null){
+        if (target == null) {
             return null;
         }
         List<Checklist> checklists = checklistRepository.findByDestinationCountry(target.getDestination().getCountry());
@@ -56,11 +66,19 @@ public class CFService {
             // 유사 사용자가 추가한 준비물 중 현재 사용자가 추가하지 않은 것만 추천
             Set<String> otherUserItems = userItemsMap.get(otherChecklistId);
             for (String item : otherUserItems) {
-                if (!currentUserItems.contains(item)) {
-                    // 기존 추천 점수가 존재하면 현재 유사도와 비교하여 더 높은 값을 저장
-                    double existingScore = recommendationScores.getOrDefault(item, 0.0);
-                    if (similarity > existingScore) {
-                        recommendationScores.put(item, similarity);
+                if (!currentUserItems.contains(item)) { // 추천 하지 않은 품목들
+                    for (String otherItem : otherUserItems) {
+                        // 'item'과 다른 'otherItem'이 다르면 유사도 계산
+                        if (!item.equals(otherItem)) {
+                            if (calculateWordMeanSimilarity(item, otherItem) <= 0.8
+                                    && calculateWordFormSimilarity(item, otherItem) >= 2) {
+                                // 기존 추천 점수가 존재하면 현재 유사도와 비교하여 더 높은 값을 저장
+                                double existingScore = recommendationScores.getOrDefault(item, 0.0);
+                                if (similarity > existingScore) {
+                                    recommendationScores.put(item, similarity);
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -86,4 +104,17 @@ public class CFService {
         // 코사인 유사도 계산
         return (double) intersectionSize / Math.sqrt(itemsA.size() * itemsB.size());
     }
+
+    private double calculateWordMeanSimilarity(String item, String otherUserItems) {
+        double similarity = wordVectors.similarity(item, otherUserItems);
+        return similarity;
+    }
+
+    private int calculateWordFormSimilarity(String item, String otherUserItems) {
+        LevenshteinDistance levenshtein = new LevenshteinDistance();
+        int distance = levenshtein.apply(item, otherUserItems);
+        return distance;
+    }
+
+
 }
